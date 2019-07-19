099249aafcfdfdf0b08bae8a7781811e64dba7ce
Yatin Aggarwal <yataggar@publicisgroupe.net>
2019-07-19 16:30:29 +0530

    product styling

--- Redux/exercise-01/src/components/Modal/Modal.js
+++ Redux/exercise-01/src/components/Modal/Modal.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Modal = (props) => {
   return (
-    <div className='backdrop'>
+    <>
+      <div className='backdrop'></div>    
       <div className='content'>
         {props.children}
         <button type="button" onClick={props.onModalClose} className="close-btn">Close</button>
-      </div>        
-    </div>
+      </div>
+    </>
   );
 }
 
--- Redux/exercise-01/src/components/Products/Product/Product.js
+++ Redux/exercise-01/src/components/Products/Product/Product.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import logo from '../../../logo.svg';
 
 const Product = (props) => {
   
   return (
     <div className="product-desc">
-      <h4>{ props.productDetail.Category }/{ props.productDetail.MainCategory }/{ props.productDetail.ProductId }</h4>
+      {props.showFullDesc ? <h4>{ props.productDetail.Category }/{ props.productDetail.MainCategory }/{ props.productDetail.ProductId }</h4> : <img src={logo} alt="product" />}
       <h2>{ props.productDetail.Name }</h2>
       { props.showFullDesc ? <p>{ props.productDetail.Description }</p> : null }
-      <span>{ props.productDetail.Price }</span>        
+      <strong><i>&#x20B9; { props.productDetail.Price.toFixed(2) }</i></strong>        
     </div>
-  )
+  );
 }
 
 export default Product;
